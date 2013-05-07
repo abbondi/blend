32b88d8fb53294a35f461db977ba2058b200694c
abbondi <magenta@massimoabbondi.it>
2013-05-07 20:53:34 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -55,12 +55,12 @@ var app = {
                     "text: " + args.text + "\n" +
                     "format: " + args.format + "\n" +
                     "cancelled: " + args.cancelled + "\n");
-                /*
+                
                 if (args.format == "QR_CODE") {
                     window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
                 }
-                */
-                document.getElementById("info").innerHTML = args.text;
+                
+                //document.getElementById("info").innerHTML = args.text;
                 console.log(args);
         });
         } catch (ex) {
